8c38d66a09d5e819410ffc74c7c264ee6c72d42c
charlievinson <93004856+charlievinson@users.noreply.github.com>
2025-05-15 19:09:58 -0700

    Add files via upload

--- Deli_Scale_Menu/Deli_Scale_Menu/src.cpp
+++ Deli_Scale_Menu/Deli_Scale_Menu/src.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 #include <map>
 #include <windows.h>
-#include <commctrl.h>
+#include <CommCtrl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
@@ -17,13 +17,48 @@
 
 #define OK_BUTTON 1100
 
-#define PRODUCT_EDITTEXT 50
+#define MENU_LABEL_EDITTEXT 50
 
-#define OPEN_PRODUCTS_BUTTON 500
+#define OPEN_PRODUCTS_BUTTON 510
+#define PRE_SLICE_BUTTON 520
+#define COLD_PROTEIN_BUTTON 530
+#define BUTTON_4 540
+#define BUTTON_5 550
+#define BUTTON_6 560
+
+#define EXIT_BUTTON 13
+
 
 using namespace std;
 
 // Global variables
+bool inPreSliceMode = false;
+
+void checkPreSliceModeFlag() {
+
+
+    string preSliceModeFlag = "";
+
+    std::ifstream pre_slice_mode_flag_file("C:/Users/Charlie/OneDrive/Desktop/c++/Deli_Scale/Deli_Scale_Main/Deli_Scale_Main/pre_slice_mode_flag.txt");
+
+    if (pre_slice_mode_flag_file.is_open())
+    {
+        string document = "";
+        while (getline(pre_slice_mode_flag_file, document))
+        {
+            preSliceModeFlag = document;
+        }
+        pre_slice_mode_flag_file.close();
+    }
+
+    if (preSliceModeFlag == "true") {
+        inPreSliceMode = true;
+    }
+    else {
+        inPreSliceMode = false;
+    }
+
+}
 
 // The main window class name.
 static TCHAR szWindowClass[] = _T("DesktopApp");
@@ -148,32 +183,145 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_CREATE:
 
     {
-        HWND hwndButtonDecreaseNumUseDays = CreateWindow(
+        HWND openProductsButton = CreateWindow(
             L"BUTTON",  // Predefined class; Unicode assumed 
             L"OPEN PRODUCTS",      // Button text 
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
-            240,         // x position 
-            285,         // y position 
-            150,        // Button width
-            80,        // Button height
+            WS_VISIBLE | WS_CHILD | BS_FLAT,  // Styles 
+            225,         // x position 
+            200,         // y position 
+            275,        // Button width
+            150,        // Button height
             hWnd,     // Parent window
             (HMENU)OPEN_PRODUCTS_BUTTON,       // No menu.
             (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
             NULL);      // Pointer not needed.
+
+        SendMessage(openProductsButton, WM_SETFONT, WPARAM(CreateFont(25, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial")), TRUE);
+
+
+        HWND preSliceButton = CreateWindow(
+            L"BUTTON",  // Predefined class; Unicode assumed 
+            L"PRE-SLICE",      // Button text 
+            WS_VISIBLE | WS_CHILD | BS_FLAT,  // Styles 
+            525,         // x position 
+            200,         // y position 
+            275,        // Button width
+            150,        // Button height
+            hWnd,     // Parent window
+            (HMENU)PRE_SLICE_BUTTON,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
+
+        SendMessage(preSliceButton, WM_SETFONT, WPARAM(CreateFont(25, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial")), TRUE);
+
+
+        HWND coldProteinButton = CreateWindow(
+            L"BUTTON",  // Predefined class; Unicode assumed 
+            L"COLD PROTEIN",      // Button text 
+            WS_VISIBLE | WS_CHILD | BS_FLAT,  // Styles 
+            825,         // x position 
+            200,         // y position 
+            275,        // Button width
+            150,        // Button height
+            hWnd,     // Parent window
+            (HMENU)COLD_PROTEIN_BUTTON,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
+
+        SendMessage(coldProteinButton, WM_SETFONT, WPARAM(CreateFont(25, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial")), TRUE);
+
+
+        HWND button4 = CreateWindow(
+            L"BUTTON",  // Predefined class; Unicode assumed 
+            L"button 4",      // Button text 
+            WS_VISIBLE | WS_CHILD | BS_FLAT,  // Styles 
+            225,         // x position 
+            400,         // y position 
+            275,        // Button width
+            150,        // Button height
+            hWnd,     // Parent window
+            (HMENU)BUTTON_4,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
+
+        HWND button5 = CreateWindow(
+            L"BUTTON",  // Predefined class; Unicode assumed 
+            L"button 5",      // Button text 
+            WS_VISIBLE | WS_CHILD | BS_FLAT,  // Styles 
+            525,         // x position 
+            400,         // y position 
+            275,        // Button width
+            150,        // Button height
+            hWnd,     // Parent window
+            (HMENU)BUTTON_5,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
+
+        HWND button6 = CreateWindow(
+            L"BUTTON",  // Predefined class; Unicode assumed 
+            L"button 6",      // Button text 
+            WS_VISIBLE | WS_CHILD | BS_FLAT,  // Styles 
+            825,         // x position 
+            400,         // y position 
+            275,        // Button width
+            150,        // Button height
+            hWnd,     // Parent window
+            (HMENU)BUTTON_6,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
+
+        /*
+        HWND menuLabelEditText = CreateWindowEx(
+            WS_EX_CLIENTEDGE,
+            _T("EDIT"),
+            _T("MENU"),
+            WS_CHILD | WS_VISIBLE | ES_READONLY,
+            30, // x position
+            20, // y position
+            1210, // width
+            70,  // height
+            hWnd,  // parent window
+            (HMENU)MENU_LABEL_EDITTEXT, // menu
+            GetModuleHandle(NULL),
+            NULL);
+
+        SendMessage(menuLabelEditText, WM_SETFONT, WPARAM(CreateFont(60, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial")), TRUE);
+        */
+
+        HWND exitButton = CreateWindow(
+            L"BUTTON",  // Predefined class; Unicode assumed 
+            L"X",      // Button text 
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+            1265,         // x position 
+            25,         // y position 
+            60,        // Button width
+            60,        // Button height
+            hWnd,     // Parent window
+            (HMENU)EXIT_BUTTON,       // No menu.
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);      // Pointer not needed.
+
+        SendMessage(exitButton, WM_SETFONT, WPARAM(CreateFont(40, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial")), TRUE);
+
     }
     case WM_COMMAND:
     {
         switch (LOWORD(wParam))
         {
-        case OK_BUTTON:
-            GetDlgItemText(hWnd, PRODUCT_EDITTEXT, editTextText, 50);
-            //MessageBox(NULL, editTextText, L"title", 0);
+        case EXIT_BUTTON:
             DestroyWindow(hWnd);
-
             break;
         case OPEN_PRODUCTS_BUTTON:
             system("C:\\Users\\Charlie\\OneDrive\\Desktop\\c++\\Deli_Scale\\Deli_Scale_Open_Products\\x64\\Debug\\Deli_Scale_Open_Products.exe");
-
+            break;
+        case PRE_SLICE_BUTTON:
+            system("C:\\Users\\Charlie\\OneDrive\\Desktop\\c++\\Deli_Scale\\Deli_Scale_Pre_Slice\\x64\\Debug\\Deli_Scale_Pre_Slice.exe");
+            checkPreSliceModeFlag();
+            if (inPreSliceMode) {
+                DestroyWindow(hWnd);
+            }
+            
+            break;
 
         }
     case WM_PAINT:
